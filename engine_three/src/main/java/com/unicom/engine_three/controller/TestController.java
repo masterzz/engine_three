0ec7ae673987397f7c5d4665da830f7372f40da1
zhubr11 <545724476@qq.com>
2018-10-17 22:30:25 +0800

    后端搭建 v1.0

---
+++ engine_three/src/main/java/com/unicom/engine_three/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.unicom.engine_three.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.unicom.engine_three.service.TestService;
+
+@Controller
+public class TestController {
+	@Autowired
+	private TestService testService;
+	
+	@RequestMapping(value="/hello")
+	@ResponseBody
+	public String hello(){
+		return "hello1";
+	}
+	
+	@RequestMapping(value="/test")
+	public String index(){
+		System.out.println("index");
+		System.out.println(testService.findById(1));
+		return "index";
+	}
+	
+	@RequestMapping(value="/test1")
+	public String test1(){
+		
+		System.out.println("test1");
+		return "test";
+	}
+}
